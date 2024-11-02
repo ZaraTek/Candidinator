1606112d8b2b493864aaf7ba1bc2072be081a4ef
malachyfernandez <jag_pyloric.0j@icloud.com>
2024-11-02 16:51:34 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
 
 function submitAddress() {
-  const address = document.getElementById("address").value;
-  const city = document.getElementById("city").value;
-  const state = document.getElementById("state").value;
-  const country = document.getElementById("country").value;
-  const zipCode = document.getElementById("zipCode").value;
+    const address = document.getElementById("address").value;
+    const city = document.getElementById("city").value;
+    const state = document.getElementById("state").value;
+    const country = "United States";
+    const zipCode = document.getElementById("zipCode").value;
+  
+    const combinedAddress = `${address}, ${city}, ${state}, ${zipCode}, ${country}`;
+    var apiKey = document.getElementById('apiKey').value;
+    var apiUrl = "https://www.googleapis.com/civicinfo/v2/voterinfo?address=" + encodeURIComponent(combinedAddress) + "&key=" + apiKey;
+  
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById('output').textContent = JSON.stringify(data, null, 2);
+            const obj = JSON.parse(data);
 
-  const combinedAddress = `${address}, ${city}, ${state}, ${zipCode}, ${country}`;
-  var apiKey = document.getElementById('apiKey').value;
-  var apiUrl = "https://www.googleapis.com/civicinfo/v2/voterinfo?address=" + encodeURIComponent(combinedAddress) + "&key=" + apiKey;
-
-  fetch(apiUrl)
-      .then(response => response.json())
-      .then(data => {
-          document.getElementById('output').textContent = JSON.stringify(data, null, 2);
-      });
-  document.getElementById('URL').textContent = apiUrl;
+            console.log(obj.name); // Output: John
+            console.log(obj.age); // Output: 30
+            console.log(obj.city); // Output: New York
+        });
+    document.getElementById('URL').textContent = apiUrl;
 }
+  
